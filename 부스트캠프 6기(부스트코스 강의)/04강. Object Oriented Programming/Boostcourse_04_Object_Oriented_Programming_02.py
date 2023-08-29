c1b63a3dc93c9f40df52899512594f886fee0bd5
LLouis <llouis0622@gmail.com>
2023-08-30 08:08:13 +0900

    .

---
+++ 부스트캠프 6기(부스트코스 강의)/04강. Object Oriented Programming/Boostcourse_04_Object_Oriented_Programming_02.py
@@ -0,0 +1,23 @@
+class Animal:
+    def__init__(self, name):
+        self.name = name
+
+
+    def talk(self):
+        raise NotImplementedError("Subclass nust implement abstract method")
+
+
+class Cat(Animal):
+    def talk(self):
+        return "Meow!"
+
+
+class Dog(Animal):
+    def talk(self):
+        return "Woof! Woof!"
+
+
+animals = [Cat('Missy'), Cat('Mr.Mistoffelees'), Dog('Lassie')]
+
+for animal in animals:
+    print(animal.name + ': ' + animal.talk())
